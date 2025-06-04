478eb1a34c926e5400ed9d453fad173e53c47585
abutlercei <abutler@ceiamerica.com>
2025-06-03 20:25:19 -0400

    Update NavigationBar.jsx

--- react-app/src/components/NavigationBar/NavigationBar.jsx
+++ react-app/src/components/NavigationBar/NavigationBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -6,53 +6,37 @@ import LayeredIcons from "../LayeredIcons/LayeredIcons";
 import styles from "./NavigationBar.module.css";
 
 export default function NavigationBar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleClick = () => {
-    if (isOpen) {
-      alert("Menu is open");
-    } else {
-      alert("Menu is closed");
-    }
-    setIsOpen((prevIsOpen) => !prevIsOpen);
-  };
-
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbarLeft}>
         <LayeredIcons />
         <p>Reality Fights</p>
       </div>
-      <div className={styles.navbarRightDesktop}>
-        <ul className={styles.navbarLinks}>
-          <li>
+      <div className={styles.navbarRight}>
+        <div className={styles.navbarRightDesktop}>
+          <ul className={styles.navbarLinks}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.navbarDropdownBox}>
+          <FontAwesomeIcon
+            className={styles.navbarRightMobile}
+            icon={faBars}
+          />
+          <div className={styles.navbarMobileMenu}>
             <Link to="/">Home</Link>
-          </li>
-          <li>
             <Link to="/search">Search</Link>
-          </li>
-          <li>
             <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-      </div>
-      <FontAwesomeIcon
-        className={styles.navbarRightMobile}
-        icon={faBars}
-        onClick={handleClick}
-      />
-      <div className={styles.navbarMobileMenu} style={{ display: "none" }}>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/search">Search</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
