571e019fe379fa923766f4f164a26c9a9670c3d9
abutlercei <abutler@ceiamerica.com>
2025-07-28 09:30:30 -0400

    Changing file names to client and server

--- start-dev.ps1
+++ start-dev.ps1
@@ -1,7 +1,7 @@
 # Runs on Windows machine with a cloned copy of GitHub repo open (https://github.com/abutlercei/RealityTvBracket.git)
 
-# Open first terminal for npm (react-app folder)
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd ./react-app; npm install; npm run dev;"
+# Open first terminal for npm (client folder)
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd ./client; npm install; npm run dev;"
 
-# Open second terminal for dotnet (DotNet folder)
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd ./DotNet; dotnet restore; dotnet run;"
+# Open second terminal for dotnet (server folder)
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd ./server; dotnet restore; dotnet run;"
